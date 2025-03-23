84cbd4209d5659d131fc3b590e81b29db0b2f9bd
nengkya <nengkya@yahoo.com>
2025-03-23 10:22:33 +0700

    django.urls.exceptions.NoReverseMatch: Reverse for '/home' not found. '/home' is not a valid view function or pattern name.

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
-# Create your tests here.
+
+#create your tests here
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text = 'class PostModelTest(TestCase):')
+
+    def test_text_content(self):
+        post = Post.objects.get(id = 1)
+
+        expected_object_name = f'{post.text}'
+
+        self.assertEqual(expected_object_name, 'class PostModelTest(TestCase):')
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text = "resp = self.client.get('/')")
+
+    def test_view_url_exist_at_proper_location(self):
+        resp = self.client.get('/')
+
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('/home'))
